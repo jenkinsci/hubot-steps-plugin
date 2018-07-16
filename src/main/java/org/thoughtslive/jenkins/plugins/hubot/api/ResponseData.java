dd4e1ed09de2db1905ae7fbb126667883a08b045
Rayapati,Naresh <naresh.rayapati@yahoo.com>
2018-07-15 20:34:52 -0500

    JENKINS-52567 Whitelist ResponseData methods in In-Process approval

--- src/main/java/org/thoughtslive/jenkins/plugins/hubot/api/ResponseData.java
+++ src/main/java/org/thoughtslive/jenkins/plugins/hubot/api/ResponseData.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.Whitelisted;
 
 /**
  * Common response that every pipeline step would receive.
@@ -21,13 +22,18 @@ public class ResponseData<T> implements Serializable {
 
   private static final long serialVersionUID = -3541969436327716610L;
 
+  @Whitelisted
   private boolean successful;
 
+  @Whitelisted
   private int code;
 
+  @Whitelisted
   private String message;
 
+  @Whitelisted
   private String error;
 
+  @Whitelisted
   private T data;
 }
